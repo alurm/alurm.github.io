8820faab4e4fc4b670bb5d21944f1446ec7ddfde
Alan Urmancheev <alan.urman@gmail.com>
2025-07-06 18:10:47 -0700

    Remove build results from the repo, add a GitHub workflow and a nix build

--- flake.nix
+++ flake.nix
@@ -7,11 +7,29 @@
   outputs = {
     nixpkgs,
     flake-utils,
-    ...
+    self,
   }:
     flake-utils.lib.eachDefaultSystem (system: let
       pkgs = nixpkgs.legacyPackages.${system};
     in {
+      packages.default = pkgs.stdenv.mkDerivation {
+        name = "alurm.github.io";
+        src = self;
+        
+        nativeBuildInputs = with pkgs; [
+          typst
+          pandoc
+          gnumake
+        ];
+
+        buildPhase = ''
+          make
+        '';
+
+        installPhase = ''
+          cp -r . $out
+        '';
+      };
       devShells.default = pkgs.mkShell {
         packages = [
           pkgs.typst
--- makefile
+++ makefile
@@ -1,9 +1,15 @@
-.DEFAULT_GOAL := default
+.DEFAULT_GOAL := all
 
 %.html: %.md
 	pandoc -i $< -o $@
 
-.PHONY: default
-default: $(patsubst %.md, %.html, $(wildcard *.md))
+.PHONY: all
+all: $(patsubst %.md, %.html, $(wildcard *.md))
 	cd resume && make
 	cd blog && make
+
+.PHONY: clean
+clean:
+	rm -f index.html
+	cd resume && make clean
+	cd blog && make clean
