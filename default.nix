ffc614b304ad4923a31d0a302b5b83c3e6315455
Alan Urmancheev <alan.urman@gmail.com>
2025-08-17 09:51:54 +0400

    Add cv.{html,pdf} links

--- default.nix
+++ default.nix
@@ -37,7 +37,11 @@ pkgs.stdenv.mkDerivation {
     cp -r "${root}/." "$out"
     cp -r "${blog}/." "$out/blog"
     cp -r "${extensions}/." "$out/extensions"
+
     cp -r "${resume}/." "$out/resume"
+
+    ln -s resume/alan-urmancheev.html "$out/cv.html"
+    ln -s resume/alan-urmancheev.pdf "$out/cv.pdf"
 
     runHook postInstall
   '';
